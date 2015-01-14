c2abd7d47931b7bfef7ee186f167214783b4d5ee
Aubrey's Macbook <Macbook@Aubrey-MacBook-Air.fritz.box>
2015-01-14 15:41:51 +0100

    First rev of Project

--- Postcardus/ViewController.swift
+++ Postcardus/ViewController.swift
@@ -9,6 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var mesageLabel: UILabel!
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    @IBOutlet weak var mailButton: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +27,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        //first line is to unhide the hidden field so that the text to be displayed shows
+        //2nd line is taking the value from our EnterMessageTextField to display in our label
+        //3rd line, makes the field empty after click on send
+        mesageLabel.hidden = false
+        mesageLabel.text = enterMessageTextField.text
+        mesageLabel.textColor = UIColor.redColor()
+        enterMessageTextField.text = ""
+        //next line makes the keyboard go away
+        enterMessageTextField.resignFirstResponder()
+        //line below changes the string Send Mail to Mail Sent 
+        mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+        
+    }
 
 }
 
